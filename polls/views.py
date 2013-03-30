7ae3768c890c804ef393f3fbddd136b1bf567b94
Prasanna Venkadesh <prasmailme@gmail.com>
2013-03-30 16:46:57 +0530

    Polling Choices visible

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,18 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from polls.models import Poll
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello World!, You are at the Poll Index')
+	latest_poll = Poll.objects.order_by('-pub_date')[:5]
+	context = {'latest_poll': latest_poll }
+	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse('You are looking at poll %s.' % poll_id)
+		poll = get_object_or_404(Poll, pk=poll_id)
+		return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
-	return HttpResponse('You are looking at poll results of poll %s.' % poll_id)
+ 	pass
 
 def vote(request, poll_id):
-	return HttpResponse('You are voting on poll %s' % poll_id)
+	pass
